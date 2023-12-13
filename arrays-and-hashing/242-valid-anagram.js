c69b049fbbe681879cccc666b7786d3e55cf401e
Andrii Andriiets <andynew3@gmail.com>
2023-12-12 18:30:13 -0800

    242-lc add processing optimized solution

--- arrays-and-hashing/242-valid-anagram.js
+++ arrays-and-hashing/242-valid-anagram.js
@@ -16,8 +16,29 @@ Output: true
  * @param {string} t
  * @return {boolean}
  */
- var isAnagram = function(s, t) {
+const isAnagram1 = (s, t) => {
   if (s.length !== t.length) return false
   
   return t.split('').sort().join('') === s.split('').sort().join('')
+};
+
+const isAnagram2 = (s, t) => {
+  if (s.length !== t.length) return false
+  const freqMap = {}
+  for (i = 0; i < s.length; i++) {
+      if (!freqMap[s[i]]) {
+          freqMap[s[i]] = 0
+      }
+      freqMap[s[i]]++
+  }
+  
+  for (i = 0; i < t.length; i++) {
+      if (!freqMap[t[i]]) {
+          return false
+      } else {
+          freqMap[t[i]]--
+      }
+  }
+
+  return true
 };
